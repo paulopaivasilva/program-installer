74d3942bc79b3d55be9edacc71b45699e01e46a4
Paulo Henrique Paiva <paulohenriquepaiva@ymail.com>
2020-02-16 01:13:54 -0300

    Commit initial
    - Layout inicial;
    - Lógica para instalar cada programa;
    - Lógica para instalar todos os programas;

---
+++ Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Programas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.arquivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.preferênciasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.opçõesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.arquivoToolStripMenuItem,
+            this.preferênciasToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(350, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // arquivoToolStripMenuItem
+            // 
+            this.arquivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sairToolStripMenuItem});
+            this.arquivoToolStripMenuItem.Name = "arquivoToolStripMenuItem";
+            this.arquivoToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.arquivoToolStripMenuItem.Text = "Arquivo";
+            // 
+            // sairToolStripMenuItem
+            // 
+            this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            this.sairToolStripMenuItem.Size = new System.Drawing.Size(93, 22);
+            this.sairToolStripMenuItem.Text = "Sair";
+            this.sairToolStripMenuItem.Click += new System.EventHandler(this.sairToolStripMenuItem_Click);
+            // 
+            // preferênciasToolStripMenuItem
+            // 
+            this.preferênciasToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.opçõesToolStripMenuItem});
+            this.preferênciasToolStripMenuItem.Name = "preferênciasToolStripMenuItem";
+            this.preferênciasToolStripMenuItem.Size = new System.Drawing.Size(83, 20);
+            this.preferênciasToolStripMenuItem.Text = "Preferências";
+            // 
+            // opçõesToolStripMenuItem
+            // 
+            this.opçõesToolStripMenuItem.Name = "opçõesToolStripMenuItem";
+            this.opçõesToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
+            this.opçõesToolStripMenuItem.Text = "Opções";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(21, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(308, 25);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Nenhum programa foi encontrado!";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(323, 17);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Adicione programas à pasta e reinicie o programa";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(138, 192);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Reiniciar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(350, 329);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Instalação de Programas";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem arquivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem preferênciasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem opçõesToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Programas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void executeProgram(string name)
+        {
+            System.Diagnostics.Process.Start(Application.StartupPath + @"\applications" + name);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            string[] directory = Directory.GetFiles(Application.StartupPath + @"\applications");
+            if(directory.Length > 0)
+            {
+                foreach (string dir in directory)
+                {
+                    int index = dir.LastIndexOf(@"\");
+                    string prog = dir.Substring(index);
+                    System.Diagnostics.Process.Start(Application.StartupPath + @"\applications" + prog).WaitForExit();
+                }
+                MessageBox.Show("Instalação completada!");
+            }
+            else
+            {
+                MessageBox.Show("Não há programas na página!");
+            }
+        }
+
+        private void sairToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] files = Directory.GetFiles(Application.StartupPath + @"\applications");
+            if(files.Length == 0)
+            {
+                this.label2.Text = "Nenhum programa foi encontrado!";
+            }
+            else
+            {
+                this.label2.Text = "";
+                this.label3.Text = "";
+                this.button1.Visible = false;
+                int top = 39;
+                int left = 130;
+
+                foreach (string file in files)
+                {
+                    int index = file.LastIndexOf(@"\");
+                    string name = file.Substring(index);
+                    int final = name.LastIndexOf(@".");
+                    Button button = new Button();
+                    button.Left = left;
+                    button.Top = top;
+                    button.Size = new Size(101, 23);
+                    button.AutoSize = true;
+                    button.Text = name.Substring(1, final - 1);
+                    button.Click += (teste, i) => this.executeProgram(name);
+                    this.Controls.Add(button);
+                    top += button.Height + 16;
+                }
+
+                Button total = new Button();
+                total.Left = left;
+                total.Top = top;
+                total.Size = new Size(101, 23);
+                total.AutoSize = true;
+                total.Text = "Instalação Total";
+                total.Click += new EventHandler(this.button7_Click);
+                this.Controls.Add(total);
+                top += total.Height + 16;
+
+                Button exit = new Button();
+                exit.Left = left;
+                exit.Top = top;
+                exit.Size = new Size(101, 23);
+                exit.AutoSize = true;
+                exit.Text = "Sair";
+                exit.Click += new EventHandler((teste, i) => { Application.Exit(); });
+                this.Controls.Add(exit);
+                top += exit.Height + 16;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+    }
+}
